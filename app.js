c52ef62b234bc3cc926330fe9424642d60b72c28
Arpit Agarwal <aamjkj@gmail.com>
2019-02-04 05:29:21 +0530

    bug fix

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ app.use(express.static('Public'));
 
 // EJS engine and index file variables
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+// app.set('view engine', 'ejs')
 app.get('/', function (req, res){
     res.render('index.ejs', {
         totalCount: 0,
         count24Hrs: 0,
         count1to7Days: 0, 
         countMoreThan7Days: 0
-    } );
+    } )
 });
 
 // Route index file
